5d30a491a86f042c2a6b7157aced3361fe13c83b
litinsky <litinsky@ipavlov.ai>
2019-07-30 16:38:57 +0300

    Impllemented TransportBus and Service classes

--- core/transport/transport.py
+++ core/transport/transport.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from core.transport.base import TransportGatewayBase, TransportConnectorBase, ServiceCallerBase
 from core.transport.adapters.rabbitmq import RabbitMQTransportGateway, RabbitMQTransportConnector
+from core.transport.z_dev_config import TRANSPORT_TYPE
 
 
 ADAPTERS_MAP = {
@@ -10,10 +13,12 @@ ADAPTERS_MAP = {
 }
 
 
+# TODO: implement services health checking
 class HealthChecker:
     pass
 
 
+# TODO: implement stateful services load balancing
 class LoadBalancer:
     pass
 
@@ -22,10 +27,21 @@ class TransportBus:
     _gateway: TransportGatewayBase
     _health_checker: HealthChecker
     _load_balancer: LoadBalancer
-    pass
+
+    def __init__(self) -> None:
+        gateway_cls = ADAPTERS_MAP[TRANSPORT_TYPE]['gateway']
+        self._gateway = gateway_cls()
+
+    async def process(self, service: str, dialog_state: dict) -> Optional[dict]:
+        return await self._gateway.process(service, dialog_state)
 
 
 class Service:
     _caller: ServiceCallerBase
     _connector: TransportConnectorBase
-    pass
+
+    def __init__(self, service_caller: ServiceCallerBase) -> None:
+        self._caller = service_caller
+
+        connector_cls = ADAPTERS_MAP[TRANSPORT_TYPE]['connector']
+        self._connector = connector_cls(self._caller)
