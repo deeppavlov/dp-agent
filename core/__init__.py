8f9540cf13ee048f5bacb5c7fda34400a512fb69
litinsky <litinsky@ipavlov.ai>
2019-08-01 20:28:33 +0300

    Added logging

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,5 @@
+from core.log import init_logger
+
 VERSION = "0.11.1"
+
+init_logger()
---
+++ core/log.py
@@ -0,0 +1,23 @@
+import logging
+import logging.config
+from pathlib import Path
+
+from core.transport.config import config
+
+
+def init_logger():
+    log_config = config['logging']
+    configured_loggers = [log_config.get('root', {})] + [logger for logger in
+                                                         log_config.get('loggers', {}).values()]
+
+    used_handlers = {handler for log in configured_loggers for handler in log.get('handlers', [])}
+
+    for handler_id, handler in list(log_config['handlers'].items()):
+        if handler_id not in used_handlers:
+            del log_config['handlers'][handler_id]
+        elif 'filename' in handler.keys():
+            filename = handler['filename']
+            logfile_path = Path(filename).expanduser().resolve()
+            handler['filename'] = str(logfile_path)
+
+    logging.config.dictConfig(log_config)
