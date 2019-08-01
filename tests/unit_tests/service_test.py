73a9a294e5a2c5c86aabb13ec0834e80eacbd7a8
litinsky <litinsky@ipavlov.ai>
2019-08-01 20:30:14 +0300

    Added Service and TransportBus unit tests

--- tests/unit_tests/service_test.py
+++ tests/unit_tests/service_test.py
@@ -1,13 +1,80 @@
+import time
+import argparse
+from uuid import uuid4
 from typing import List
+from copy import deepcopy
 
-from core.transport.config import config
+from core.transport.config import config as service_config
 from core.transport.transport import Service
 from core.transport.base import ServiceCallerBase
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--service', default='foo', help='service name', type=str)
+parser.add_argument('-i', '--instance', default='bar', help='service instance id', type=str)
+parser.add_argument('-b', '--batch-size', default=3, help='batch size', type=int)
+
+
+STATE_EXAMPLE = {
+    'task': {
+        'agent_name': '',
+        'service': '',
+        'sleep_time': 0.0,
+        'utterance': ''
+    },
+    'response': {
+        'agent_name': '',
+        'service': '',
+        'service_instance_id': '',
+        'batch_id': '',
+        'sleep_time': 0.0,
+        'response': ''
+    }
+}
+
+
 class MockServiceCaller(ServiceCallerBase):
-    def infer(self, dialog_states_batch: List[dict]) -> None:
+    _config: dict
+    _service_name: str
+    _instance_id: str
+
+    def __init__(self, config) -> None:
+        self._config = config
+        self._service_name = self._config['service']['name']
+        self._instance_id = self._config['service']['instance_id']
+
+    def infer(self, dialog_states_batch: List[dict]) -> List[dict]:
+        sleep_time_batch = []
+        batch_id = str(uuid4())
+
+        for dialog_state in dialog_states_batch:
+            dialog_state['response'] = {}
+            dialog_state['response']['agent_name'] = dialog_state['task']['agent_name']
+            dialog_state['response']['service'] = self._service_name
+            dialog_state['response']['service_instance_id'] = self._instance_id
+            dialog_state['response']['batch_id'] = batch_id
+            dialog_state['response']['sleep_time'] = dialog_state['task']['sleep_time']
+            dialog_state['response']['response'] = f"{dialog_state['task']['utterance']}_responded"
+            sleep_time_batch.append(dialog_state['task']['sleep_time'])
+
+        sleep_time = max(sleep_time_batch)
+        time.sleep(sleep_time)
+
+        return dialog_states_batch
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    service_name = args.service
+    instance_id = args.instance
+    batch_size = args.batch_size
+
+    conf = deepcopy(service_config)
+    conf['service']['name'] = service_name
+    conf['service']['instance_id'] = instance_id
+    conf['service']['batch_size'] = batch_size
+
+    try:
+        service = Service(config=conf, service_caller=MockServiceCaller(conf))
+    except KeyboardInterrupt:
         pass
-
-
-service = Service(config=config, service_caller=MockServiceCaller())
--- tests/unit_tests/transport_bus_test.py
+++ tests/unit_tests/transport_bus_test.py
@@ -1,5 +1,71 @@
-from core.transport.config import config
+import argparse
+import asyncio
+from copy import deepcopy
+
+from core.transport.config import config as agent_config
 from core.transport.transport import TransportBus
 
 
-transport_bus = TransportBus(config=config)
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--name', default='fizz_buzz', help='agent name', type=str)
+parser.add_argument('-s', '--service', default='foo', help='service name', type=str)
+parser.add_argument('-t', '--timeout', default=8.0, help='transport timeout', type=float)
+
+
+STATE_EXAMPLE = {
+    'task': {
+        'agent_name': '',
+        'service': '',
+        'sleep_time': 0.0,
+        'utterance': ''
+    },
+    'response': {
+        'agent_name': '',
+        'service': '',
+        'service_instance_id': '',
+        'batch_id': '',
+        'sleep_time': 0.0,
+        'response': ''
+    }
+}
+
+
+async def send(transport_bus: TransportBus, service: str, task: dict) -> None:
+    result = await transport_bus.process(service=service, dialog_state=task)
+    result = result or task
+    print(f'TEST RESULT: {result}')
+
+
+async def test(transport_bus: TransportBus, agent_name: str, service_name: str) -> None:
+    test_case = [
+        {'task': {'agent_name': agent_name, 'service': service_name, 'sleep_time': 3.0, 'utterance': 'utt'}},
+        {'task': {'agent_name': agent_name, 'service': service_name, 'sleep_time': 3.0, 'utterance': 'utt'}},
+        {'task': {'agent_name': agent_name, 'service': service_name, 'sleep_time': 3.0, 'utterance': 'utt'}},
+        {'task': {'agent_name': agent_name, 'service': service_name, 'sleep_time': 3.0, 'utterance': 'utt'}},
+        {'task': {'agent_name': agent_name, 'service': service_name, 'sleep_time': 3.0, 'utterance': 'utt'}}
+    ]
+
+    print(f'Test tasks:\n' + '\n'.join([str(task) for task in test_case]))
+    tasks = [send(transport_bus, task['task']['service'], task) for task in test_case]
+    await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    agent_name = args.name
+    service_name = args.service
+    timeout = args.timeout
+
+    conf = deepcopy(agent_config)
+    conf['agent']['name'] = agent_name
+    conf['transport']['timeout_sec'] = timeout
+
+    mock_agent = TransportBus(config=conf)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(test(mock_agent, agent_name, service_name))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
