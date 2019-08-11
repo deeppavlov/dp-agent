3147a8bc0363a355c637acf26d6109a0072bcf38
litinsky <alitinsky@gmail.com>
2019-08-11 21:50:04 +0300

    core/connection.py now uses agent_orange config

--- core/connection.py
+++ core/connection.py
@@ -1,6 +1,9 @@
 from mongoengine import connect
+from agent_orange.config import config
 
-from core.config import HOST, PORT, DB_NAME
 
+db_host = config['agent']['database']['host']
+db_port = config['agent']['database']['port']
+db_name = f'{config["agent_namespace"]}_{config["agent"]["name"]}'
 
-state_storage = None # (connect(host=HOST, port=PORT, db=DB_NAME)
+state_storage = connect(host=db_host, port=db_port, db=db_name)
