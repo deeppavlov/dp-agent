a898da17106868144975d16ff570cf771027b22f
litinsky <alitinsky@gmail.com>
2019-08-11 21:31:13 +0300

    Updated config verification

--- agent_orange/config.py
+++ agent_orange/config.py
@@ -18,15 +18,25 @@ def verify_pipeline(cfg: dict) -> None:
     pipeline = cfg['agent']['pipeline']
     type_error_text = 'Pipeline elements should be of str or List[str] types'
 
+    if not isinstance(pipeline, list):
+        raise TypeError(ERR_MSG_TEMPLATE.format('Pipeline should be list instance'))
+
+    if len(pipeline) == 0:
+        raise ValueError(ERR_MSG_TEMPLATE.format('Pipeline should not be empty'))
+
     for stage in pipeline:
         if isinstance(stage, list):
             if not all(isinstance(service, str) for service in stage):
                 raise TypeError(ERR_MSG_TEMPLATE.format(type_error_text))
+
+            if len(stage) == 0:
+                raise ValueError(ERR_MSG_TEMPLATE.format('Services groups should not be empty'))
+
         elif not isinstance(stage, str):
             raise TypeError(ERR_MSG_TEMPLATE.format(type_error_text))
 
     if not isinstance(pipeline[-1], str):
-        raise TypeError(ERR_MSG_TEMPLATE.format('Pipeline last element should be only one service'))
+        raise TypeError(ERR_MSG_TEMPLATE.format('Pipeline last element should be only one service, not group'))
 
 
 verify(config)
