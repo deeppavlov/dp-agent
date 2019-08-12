2297fb8e21222f7471d34a26ee0bd6ca06fb6b50
litinsky <alitinsky@gmail.com>
2019-08-13 00:14:48 +0300

    Changed some naming

--- agent_orange/tests/unit_tests/transport_gateway_test.py
+++ agent_orange/tests/unit_tests/transport_gateway_test.py
@@ -46,7 +46,8 @@ class MockAgent:
         print(f'RECEIVED STATE {str(current_time)} {str(message)}')
 
     async def send(self, dialog_state: dict) -> None:
-        await self._transport_gateway.process(service=dialog_state['task']['service'], dialog_state=dialog_state)
+        await self._transport_gateway.send_to_service(service=dialog_state['task']['service'],
+                                                      dialog_state=dialog_state)
 
 
 async def test(mock_agent: MockAgent, agent_name: str, service_name: str) -> None:
@@ -66,16 +67,16 @@ async def test(mock_agent: MockAgent, agent_name: str, service_name: str) -> Non
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    agent_name = args.name
-    service_name = args.service
+    _agent_name = args.name
+    _service_name = args.service
 
     conf = deepcopy(agent_config)
-    conf['agent']['name'] = agent_name
+    conf['agent']['name'] = _agent_name
 
-    mock_agent = MockAgent(config=conf)
+    _mock_agent = MockAgent(config=conf)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(test(mock_agent, agent_name, service_name))
+    loop.create_task(test(_mock_agent, _agent_name, _service_name))
 
     try:
         loop.run_forever()
