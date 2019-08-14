f0778ef0ee9de5d81b9841f4265a87d6453ec30b
litinsky <alitinsky@gmail.com>
2019-08-14 10:58:13 +0300

    Added stand ner deeppavlov formatter

--- connectors/formatters/__init__.py
+++ connectors/formatters/__init__.py
@@ -1,10 +1,15 @@
-from connectors.formatters.deeppavlov import format_dp_ner
+from connectors.formatters.deeppavlov import format_dp_ner, format_dp_ner_stand
 from connectors.formatters.agent import format_agent_ranking_chitchat_prep
 
 formatters_map = {
     'deeppavlov_ner': {
         'formatter': format_dp_ner,
         'description': 'Adapter to DeepPavlov named entity recognition models',
+        'default_caller': 'simple_http_caller'
+    },
+    'deeppavlov_ner_stand': {
+        'formatter': format_dp_ner_stand,
+        'description': 'Adapter to DeepPavlov named entity recognition models, demo stand API',
         'default_caller': 'simple_http_caller'
     },
     'agent_ranking_chitchat_prep': {
--- connectors/formatters/deeppavlov.py
+++ connectors/formatters/deeppavlov.py
@@ -25,3 +25,30 @@ def format_dp_ner(data: Union[List[Dict], List[List[List[str]]]], out: bool) -> 
         payload['context'] = utterance_texts
 
         return payload
+
+
+def format_dp_ner_stand(data: Union[List[Dict], List[List[List[str]]]],
+                        out: bool) -> Union[Dict[str, List[str]], List[Dict]]:
+    if out:
+        annotations = []
+
+        for response in data:
+            annotation = {}
+            annotation['tokens'] = response[0]
+            annotation['tags'] = response[1]
+            annotations.append(annotation)
+
+        return annotations
+
+    else:
+        payload = {}
+        utterance_texts = []
+
+        for dialog_state in data:
+            last_utterance = dialog_state['utterances'][-1]
+            utterance_text = last_utterance['text']
+            utterance_texts.append(utterance_text)
+
+        payload['text1'] = utterance_texts
+
+        return payload
