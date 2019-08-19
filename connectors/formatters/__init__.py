3a16a388e513a6bb49623a735d894c2f92255164
litinsky <alitinsky@gmail.com>
2019-08-20 01:20:40 +0300

    Added formatters, switched to stand ner

--- connectors/formatters/__init__.py
+++ connectors/formatters/__init__.py
@@ -1,5 +1,5 @@
-from connectors.formatters.deeppavlov import format_dp_ner, format_dp_ner_stand
-from connectors.formatters.agent import format_agent_ranking_chitchat_prep
+from connectors.formatters.deeppavlov import format_dp_ner, format_dp_ner_stand, format_dp_odqa_stand
+from connectors.formatters.agent import format_agent_ranking_chitchat_prep, format_chitchat_odqa_selector
 
 formatters_map = {
     'deeppavlov_ner': {
@@ -12,9 +12,19 @@ formatters_map = {
         'description': 'Adapter to DeepPavlov named entity recognition models, demo stand API',
         'default_caller': 'simple_http_caller'
     },
+    'deeppavlov_odqa_stand': {
+        'formatter': format_dp_odqa_stand,
+        'description': 'Adapter to DeepPavlov ODQA models, demo stand API',
+        'default_caller': 'simple_http_caller'
+    },
     'agent_ranking_chitchat_prep': {
         'formatter': format_agent_ranking_chitchat_prep,
         'description': 'Adapter to legacy Agent ranking chitchat',
+        'default_caller': 'simple_http_caller'
+    },
+    'agent_chitchat_odqa_selector': {
+        'formatter': format_chitchat_odqa_selector,
+        'description': 'Adapter to legacy Agent chitchat-ODQA skill selector',
         'default_caller': 'simple_http_caller'
     }
 }
--- connectors/formatters/deeppavlov.py
+++ connectors/formatters/deeppavlov.py
@@ -52,3 +52,31 @@ def format_dp_ner_stand(data: Union[List[Dict], List[List[List[str]]]],
         payload['text1'] = utterance_texts
 
         return payload
+
+
+def format_dp_odqa_stand(data: Union[List[Dict], List[List[str]]],
+                        out: bool) -> Union[Dict[str, List[str]], List[Dict]]:
+    if out:
+        service_responses = []
+
+        for response in data:
+            service_response = {}
+            service_response['text'] = response[0]
+            service_response['confidence'] = response[1]
+            service_response['name'] = None
+            service_responses.append(service_response)
+
+        return service_responses
+
+    else:
+        payload = {}
+        utterance_texts = []
+
+        for dialog_state in data:
+            last_utterance = dialog_state['utterances'][-1]
+            utterance_text = last_utterance['text']
+            utterance_texts.append(utterance_text)
+
+        payload['text1'] = utterance_texts
+
+        return payload
