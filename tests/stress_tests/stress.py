d6d500b64bcbea7da5534d6d68b031b5de52f0aa
ignatov <ignatov.fedor@gmail.com>
2019-08-30 10:39:34 +0300

    refactor: gateway moved into connector

--- tests/stress_tests/stress.py
+++ tests/stress_tests/stress.py
@@ -5,14 +5,13 @@ import uuid
 from collections import defaultdict
 from pathlib import Path
 from time import time
-from typing import Awaitable, Callable, Dict
 
 import yaml
 import numpy as np
 
 from core.config import get_config
 from core.transport import transport_map
-from core.transport.base import ChannelConnectorBase, TChannelConnector, TChannelGateway
+from core.transport.base import TChannelGateway
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--utters-number', help='number of infers to send', type=int)
@@ -33,11 +32,15 @@ config['channel'] = config['channels'][channel_id] = {'id': channel_id}
 loop = asyncio.get_event_loop()
 
 
-class StressConnector(ChannelConnectorBase):
-    def __init__(self, config: dict, on_channel_callback: Callable[[str, str, str, bool], Awaitable]) -> None:
-        super(StressConnector, self).__init__(config=config, on_channel_callback=on_channel_callback)
+class StressConnector:
+    def __init__(self) -> None:
         self.utterances = defaultdict(dict)
         self.event = asyncio.Event()
+
+        transport_type = config['transport']['type']
+        gateway_cls = transport_map[transport_type]['channel']
+
+        self._gateway: TChannelGateway = gateway_cls(config=config, to_channel_callback=self.send_to_channel)
 
     async def send_data(self, utters, utters_number) -> None:
         for _ in range(utters_number):
@@ -45,10 +48,10 @@ class StressConnector(ChannelConnectorBase):
             _user_id = str(uuid.uuid4())
             self.utterances['request'][_user_id] = {'send': utterance,
                                                     'send_time': time()}
-            loop.create_task(self._on_channel_callback(utterance=utterance,
-                                                       channel_id=self._channel_id,
-                                                       user_id=_user_id,
-                                                       reset_dialog=False))
+            loop.create_task(self._gateway.send_to_agent(utterance=utterance,
+                                                         channel_id=channel_id,
+                                                         user_id=_user_id,
+                                                         reset_dialog=False))
 
     async def send_to_channel(self, user_id: str, response: str) -> None:
         utter = self.utterances['request'].pop(user_id)
@@ -57,24 +60,6 @@ class StressConnector(ChannelConnectorBase):
         self.utterances['response'][user_id] = utter
         if not self.utterances['request']:
             self.event.set()
-
-
-def run_channel(config: Dict):
-    async def on_channel_message(utterance: str, channel_id: str, user_id: str, reset_dialog: bool) -> None:
-        await _gateway.send_to_agent(utterance=utterance,
-                                     channel_id=channel_id,
-                                     user_id=user_id,
-                                     reset_dialog=reset_dialog)
-
-    async def send_to_channel(user_id: str, response: str) -> None:
-        await _channel_connector.send_to_channel(user_id=user_id, response=response)
-
-    transport_type = config['transport']['type']
-    gateway_cls = transport_map[transport_type]['channel']
-
-    _gateway: TChannelGateway = gateway_cls(config=config, to_channel_callback=send_to_channel)
-    _channel_connector: TChannelConnector = StressConnector(config=config, on_channel_callback=on_channel_message)
-    return _channel_connector
 
 
 def process_results(utts):
@@ -96,7 +81,6 @@ async def foo(connector, utters, utters_number):
 
 def main():
     args = parser.parse_args()
-    print(args)
     utters_number = args.utters_number
     utters_type = args.utters_type
     if utters_type == '':
@@ -105,8 +89,8 @@ def main():
             utters += utter_list
     else:
         utters = stress_config['utters'][utters_type]
-    connector = run_channel(config)
-    loop.run_until_complete(foo(connector, utters, utters_number))
+    _channel_connector: StressConnector = StressConnector()
+    loop.run_until_complete(foo(_channel_connector, utters, utters_number))
 
 
 if __name__ == '__main__':
