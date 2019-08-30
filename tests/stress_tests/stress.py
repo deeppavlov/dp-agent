9bfc67ad7ff6b87e8e8de5f1a6525cbccff2acc9
ignatov <ignatov.fedor@gmail.com>
2019-08-30 15:10:43 +0300

    feat: added logging to strress test

--- tests/stress_tests/stress.py
+++ tests/stress_tests/stress.py
@@ -2,9 +2,11 @@ import argparse
 import asyncio
 import random
 import uuid
-from collections import defaultdict
+import logging
 from pathlib import Path
-from time import time
+from datetime import datetime
+from logging import config as log_config
+from typing import Optional
 
 import yaml
 import numpy as np
@@ -13,8 +15,18 @@ from core.config import get_config
 from core.transport import transport_map
 from core.transport.base import TChannelGateway
 
+
+def positive_int(value: str) -> int:
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(f'invalid positive_int value: {value}')
+    return ivalue
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--utters-number', help='number of infers to send', type=int)
+parser.add_argument('-b', '--batch-size', help='number of users simultaneously sending a message', type=positive_int)
+parser.add_argument('-i', '--infers-num', help='number of infers by which the response time is averaged',
+                    type=positive_int, default=5)
 parser.add_argument('-t', '--utters-type', type=str, choices={'odqa', 'chitchat'}, default='')
 
 
@@ -28,69 +40,74 @@ config = get_config(root_dir / stress_config['config_path'])
 
 channel_id = 'tests'
 config['channel'] = config['channels'][channel_id] = {'id': channel_id}
+transport_type = config['transport']['type']
+gateway_cls = transport_map[transport_type]['channel']
+
+stress_config['logging']['handlers']['log_to_file']['filename'] = datetime.now().strftime(stress_config['log_file_name_format'])
+log_config.dictConfig(stress_config["logging"])
+log = logging.root.manager.loggerDict['stress_logger']
 
 loop = asyncio.get_event_loop()
 
 
 class StressConnector:
-    def __init__(self) -> None:
-        self.utterances = defaultdict(dict)
-        self.event = asyncio.Event()
-
-        transport_type = config['transport']['type']
-        gateway_cls = transport_map[transport_type]['channel']
-
+    def __init__(self, utters_type: str) -> None:
+        self._responses_left = None
+        self._event = asyncio.Event()
+        self._lock = asyncio.Lock()
         self._gateway: TChannelGateway = gateway_cls(config=config, to_channel_callback=self.send_to_channel)
+        self._phrases_list = []
+        keys = stress_config['phrases'].keys() if utters_type == '' else [utters_type]
+        for key in keys:
+            self._phrases_list += stress_config['phrases'][key]
 
-    async def send_data(self, utters, utters_number) -> None:
-        for _ in range(utters_number):
-            utterance = random.choice(utters)
-            _user_id = str(uuid.uuid4())
-            self.utterances['request'][_user_id] = {'send': utterance,
-                                                    'send_time': time()}
+    async def run_test(self, batch_size: int, infers_num: int) -> None:
+        faults = 0
+        test_durations = []
+        for _ in range(infers_num):
+            test_duration = await self._infer_data(batch_size)
+            if test_duration is None:
+                faults += 1
+            else:
+                test_durations.append(test_duration)
+        avg_time = std_time = 0
+        if test_durations:
+            avg_time = np.average(test_durations)
+            std_time = np.std(test_durations)
+        log.info(f'batch_size: {batch_size}, infers_num: {infers_num}, FAULTS: {faults}, AVG_TIME: {avg_time:.2f}, STD: {std_time:.2f}')
+
+    async def _infer_data(self, batch_size: int) -> Optional[float]:
+        utters_list = [random.choice(self._phrases_list) for _ in range(batch_size)]
+        self._responses_left = batch_size
+        self._event.clear()
+        batch_send_time = datetime.now()
+        for utterance in utters_list:
             loop.create_task(self._gateway.send_to_agent(utterance=utterance,
                                                          channel_id=channel_id,
-                                                         user_id=_user_id,
+                                                         user_id=str(uuid.uuid4()),
                                                          reset_dialog=False))
+        try:
+            await asyncio.wait_for(self._event.wait(), timeout=stress_config['test_timeout'])
+        except asyncio.TimeoutError:
+            pass
+        else:
+            test_time = (datetime.now() - batch_send_time).total_seconds()
+            return test_time
 
     async def send_to_channel(self, user_id: str, response: str) -> None:
-        utter = self.utterances['request'].pop(user_id)
-        utter['resp_time'] = time()
-        utter['response'] = response
-        self.utterances['response'][user_id] = utter
-        if not self.utterances['request']:
-            self.event.set()
-
-
-def process_results(utts):
-    resp = utts['response']
-    req = utts['request']
-    dt = [utt['resp_time'] - utt['send_time'] for utt in resp.values()]
-    print(f'{len(req)} errors\nAverage response time: {np.average(dt)}\nStandart deviation: {np.std(dt)}')
-
-
-async def foo(connector, utters, utters_number):
-    await connector.send_data(utters, utters_number)
-    try:
-        await asyncio.wait_for(connector.event.wait(), timeout=stress_config['test_timeout'])
-    except asyncio.TimeoutError:
-        print('Timeout error')
-    finally:
-        process_results(connector.utterances)
+        async with self._lock:
+            self._responses_left -= 1
+        if self._responses_left == 0:
+            self._event.set()
 
 
 def main():
     args = parser.parse_args()
-    utters_number = args.utters_number
+    batch_size = args.batch_size
     utters_type = args.utters_type
-    if utters_type == '':
-        utters = []
-        for utter_list in stress_config['utters'].values():
-            utters += utter_list
-    else:
-        utters = stress_config['utters'][utters_type]
-    _channel_connector: StressConnector = StressConnector()
-    loop.run_until_complete(foo(_channel_connector, utters, utters_number))
+    infers_num = args.infers_num
+    _channel_connector: StressConnector = StressConnector(utters_type)
+    loop.run_until_complete(_channel_connector.run_test(batch_size, infers_num))
 
 
 if __name__ == '__main__':
