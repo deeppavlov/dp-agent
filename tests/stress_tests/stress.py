6e94305b3467434e9c19a79aa96c760571369c2a
ignatov <ignatov.fedor@gmail.com>
2019-09-02 15:27:56 +0300

    feat: added convai infers

--- tests/stress_tests/stress.py
+++ tests/stress_tests/stress.py
@@ -1,123 +1,155 @@
-import argparse
 import asyncio
-import random
+import bisect
 import logging
-from pathlib import Path
+import statistics
+import random
+from collections import defaultdict
 from datetime import datetime
 from logging import config as log_config
-from typing import List, Optional
+from pathlib import Path
+from typing import List, Tuple
 from uuid import uuid4
-
-import numpy as np
-import yaml
 
 from core.agent import TIMEOUT_MESSAGE
 from core.config import get_config
 from core.transport import transport_map
 from core.transport.base import TChannelGateway
-
-
-def positive_int(value: str) -> int:
-    ivalue = int(value)
-    if ivalue <= 0:
-        raise argparse.ArgumentTypeError(f'invalid positive_int value: {value}')
-    return ivalue
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--batch-size', help='number of users simultaneously sending a message', type=positive_int)
-parser.add_argument('-i', '--infers-num', help='number of infers by which the response time is averaged',
-                    type=positive_int, default=5)
-parser.add_argument('-t', '--utters-type', type=str, choices={'odqa', 'chitchat'}, default='')
-
-
-root_dir = Path(__file__).resolve().parents[2]
-stress_config_path = root_dir / 'tests/stress_tests/stress_config.yaml'
-
-with stress_config_path.open('r') as f:
-    stress_config = yaml.safe_load(f)
-
-config = get_config(root_dir / stress_config['config_path'])
-
-log_config.dictConfig(stress_config["logging"])
-log = logging.root.manager.loggerDict['stress_logger']
+from tests.stress_tests.test_config import test_config
 
 loop = asyncio.get_event_loop()
 
 
-class StressConnector:
-    def __init__(self, config: dict, phrases_list: List[str]) -> None:
+class StressTestConnector:
+    def __init__(self, config: dict) -> None:
         self._channel_id = 'tests'
-        self._wrong_response = False
+        self._infer_is_successful = True
+        self._user_ids = list()
+
         config['channel'] = config['channels'][self._channel_id] = {'id': self._channel_id}
         transport_type = config['transport']['type']
         gateway_cls = transport_map[transport_type]['channel']
 
-        self._responses_left = None
-        self._event = asyncio.Event()
+        self._responses_left = 0
+        self._got_all_responses = asyncio.Event()
         self._gateway: TChannelGateway = gateway_cls(config=config, to_channel_callback=self.send_to_channel)
-        self._phrases_list = phrases_list
+        self._utterance_generator = UtteranceGenerator(test_config['dialogs_abs_path'])
 
-    async def run_test(self, batch_size: int, infers_num: int) -> None:
-        faults = 0
-        test_durations = []
+    def run_test(self, batch_size: int, utt_length: int = 5, infers_num: int = 1) -> Tuple[int, float, float]:
+        results = list()
         for _ in range(infers_num):
-            test_duration = await self._infer_data(batch_size)
-            if test_duration is None:
-                faults += 1
-            else:
-                test_durations.append(test_duration)
-        avg_time = std_time = 0
-        if test_durations:
-            avg_time = np.average(test_durations)
-            std_time = np.std(test_durations)
-        log.info(f'batch_size: {batch_size}, infers_num: {infers_num}, FAULTS: {faults}, '
-                 f'AVG_TIME: {avg_time:.2f}, STD: {std_time:.2f}')
+            utters_list = [self._utterance_generator(utt_length) for _ in range(batch_size)]
+            self._user_ids = [str(uuid4()) for _ in range(batch_size)]
+            self._responses_left = batch_size
+            self._got_all_responses.clear()
+            self._infer_is_successful = True
+            results.append(loop.run_until_complete(self._infer_data(utters_list)))
 
-    async def _infer_data(self, batch_size: int) -> Optional[float]:
-        self._wrong_response = False
-        utters_list = [(random.choice(self._phrases_list)) for _ in range(batch_size)]
-        self._users_list = [str(uuid4()) for _ in range(batch_size)]
-        self._responses_left = batch_size
-        self._event.clear()
-        batch_send_time = datetime.now()
-        for user_id, utterance in zip(self._users_list, utters_list):
+        passed, await_times = list(zip(*results))
+        faults_num = passed.count(False)
+        await_times = [infer[1] for infer in results if infer[0] is True]
+
+        await_avg = statistics.mean(await_times) if await_times else 0
+        await_std = statistics.stdev(await_times) if len(await_times) >= 2 else 0
+
+        return faults_num, await_avg, await_std
+
+    async def _infer_data(self, utterances: List[str]) -> Tuple[bool, float]:
+        time_begin = loop.time()
+        for user_id, utterance in zip(self._user_ids, utterances):
             loop.create_task(self._gateway.send_to_agent(utterance=utterance,
                                                          channel_id=self._channel_id,
                                                          user_id=user_id,
                                                          reset_dialog=False))
         try:
-            await asyncio.wait_for(self._event.wait(), timeout=stress_config['test_timeout'])
+            await asyncio.wait_for(self._got_all_responses.wait(), timeout=test_config['infer_timeout'])
         except asyncio.TimeoutError:
-            pass
-        else:
-            if not self._wrong_response:
-                test_time = (datetime.now() - batch_send_time).total_seconds()
-                return test_time
+            self._infer_is_successful = False
+
+        return self._infer_is_successful, loop.time() - time_begin
 
     async def send_to_channel(self, user_id: str, response: str) -> None:
-        self._responses_left -= 1
         if response == TIMEOUT_MESSAGE:
-            log.debug('got timeout message')
-            self._wrong_response = True
-        elif user_id not in self._users_list:
-            log.debug('got unexpected user_id')
-            self._wrong_response = True
-        if self._responses_left == 0 or self._wrong_response is True:
-            self._event.set()
+            self._infer_is_successful = False
+
+        if user_id in self._user_ids:
+            self._responses_left -= 1
+
+        if self._responses_left == 0:
+            self._got_all_responses.set()
+
+
+class UtteranceGenerator:
+    def __init__(self, dialogs_abs_path: str) -> None:
+        dialogs_abs_path = Path(dialogs_abs_path).resolve()
+        with dialogs_abs_path.open('r') as f:
+            dialogs_str: str = f.read()
+            self._examples = defaultdict(set)
+
+            tokens = dialogs_str.replace('\n\n', ' ').replace('\n', ' ').split(' ')
+            replicas = [replica for dialog in dialogs_str.split('\n\n') for replica in dialog.split('\n')]
+            dialogs = [dialog.replace('\n', ' ') for dialog in dialogs_str.split('\n\n')]
+
+            for token in tokens:
+                self._examples[len(token)].add(token)
+
+            for repl in replicas:
+                self._examples[len(repl)].add(repl)
+
+            for dial in dialogs:
+                self._examples[len(dial)].add(dial)
+
+            for key, value in self._examples.items():
+                self._examples[key] = list(self._examples[key])
+
+            self._indexes = list(self._examples.keys())
+            self._indexes.sort()
+
+    def __call__(self, symbols_num: int) -> str:
+        i = bisect.bisect_right(self._indexes, symbols_num)
+        substring_symbols_num = self._indexes[i - 1] if i > 0 else 1
+        substring = random.choice(self._examples.get(substring_symbols_num, ['!']))
+        symbols_num_delta = symbols_num - substring_symbols_num
+
+        if symbols_num_delta < 1:
+            result = substring
+        elif symbols_num_delta == 1:
+            result = f'{substring}{self.__call__(symbols_num_delta)}'
+        else:
+            result = f'{substring} {self.__call__(symbols_num_delta - 1)}'
+
+        return result
 
 
 def main():
-    args = parser.parse_args()
-    batch_size = args.batch_size
-    utters_type = args.utters_type
-    infers_num = args.infers_num
-    phrases_list = []
-    keys = stress_config['phrases'].keys() if utters_type == '' else [utters_type]
-    for key in keys:
-        phrases_list += stress_config['phrases'][key]
-    _channel_connector: StressConnector = StressConnector(config, phrases_list)
-    loop.run_until_complete(_channel_connector.run_test(batch_size, infers_num))
+    root_dir = Path(__file__).resolve().parents[2]
+
+    config = get_config(root_dir / test_config['config_path'])
+
+    logs_dir = Path(__file__).resolve().parent / 'logs'
+    logs_dir.mkdir(exist_ok=True)
+
+    test_config['logging']['handlers']['log_to_file']['filename'] = logs_dir / f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')}_test.log"
+    log_config.dictConfig(test_config["logging"])
+    log = logging.root.manager.loggerDict['stress_logger']
+    channel_connector: StressTestConnector = StressTestConnector(config)
+    for test in test_config['tests']:
+        log.info(f'Starting {test["test_name"]}')
+
+        batch_size = test['batch_size']
+        utt_length = test['utt_length']
+        infers_num = test['infers_num']
+
+        batch_size = list(range(batch_size, batch_size + 1, 1)) if isinstance(batch_size, int) else list(batch_size)
+        utt_length = list(range(utt_length, utt_length + 1, 1)) if isinstance(utt_length, int) else list(utt_length)
+        infers_num = list(range(infers_num, infers_num + 1, 1)) if isinstance(infers_num, int) else list(infers_num)
+
+        test_grid = [(bs, ul, inum) for bs in batch_size for ul in utt_length for inum in infers_num]
+
+        for bs, ul, inum in test_grid:
+            faults_num, await_avg, await_std = channel_connector.run_test(bs, ul, inum)
+
+            log.info(f'batch_size: {bs}, utt_length: {ul}, infers_num: {inum}, '
+                     f'FAULTS: {faults_num}, AVG_TIME: {await_avg}, STD {await_std}')
 
 
 if __name__ == '__main__':
---
+++ tests/stress_tests/test_config.py
@@ -0,0 +1,56 @@
+log_config = {
+    'version': 1,
+    'formatters': {
+        'log_formatter': {
+            'class': 'logging.Formatter',
+            'format': '%(asctime)s %(message)s'
+        }
+    },
+    'handlers': {
+        'log_to_stderr': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stderr',
+            'formatter': 'log_formatter'
+        },
+        'log_to_file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'DEFAULT_NAME.log',
+            'formatter': 'log_formatter',
+            'mode': 'w'
+        }
+    },
+    'loggers': {
+        'stress_logger': {
+            'propagate': False,
+            'level': 'DEBUG',
+            'handlers': ['log_to_stderr',
+                         'log_to_file']
+        }
+    }
+}
+
+tests = {
+    'max_batch_size': {
+        'test_name': 'max_batch_size',
+        'batch_size': list(range(10, 201, 10)),
+        'utt_length': 20,
+        'infers_num': 1
+    },
+    'max_string_length_batch_1': {
+        'test_name': 'max_string_length_batch_1',
+        'batch_size': 1,
+        'utt_length': list(range(50, 100, 50)),
+        'infers_num': 1
+    }
+}
+
+test_config = {
+  'config_path': 'deployment/ner_chitchat_local/run_config.yaml',
+  'dialogs_url': 'https://raw.githubusercontent.com/deepmipt/agent_stress_test/dev/dialogs.txt',
+  'infer_timeout': 300,
+  'logging': log_config,
+  'tests': [tests['max_batch_size'],
+            tests['max_string_length_batch_1']]
+}
