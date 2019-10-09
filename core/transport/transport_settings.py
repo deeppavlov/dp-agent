eecf5508b9eb31d882c1b3f9c724686d8fbe9271
litinsky <litinsky@ipavlov.ai>
2019-10-09 14:44:01 +0300

    Fixed proper utterance_lifetime_sec param name

--- core/transport/transport_settings.py
+++ core/transport/transport_settings.py
@@ -1,7 +1,7 @@
 TRANSPORT_SETTINGS = {
     'agent_namespace': 'deeppavlov_agent',
     'agent_name': 'dp_agent',
-    'response_timeout_sec': 120,
+    'utterance_lifetime_sec': 120,
     'channels': {},
     'transport': {
         'type': 'AMQP',
