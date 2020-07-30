1fda7c65e82a91b1bf3c1f63cebbc4e64d62235c
Денис Кузнецов <kuznetsov.den.p@gmail.com>
2020-07-30 16:07:05 +0300

    fix (hotfix/origin_text): add origin text into add_bot_utterance_last_chance_overwrite

--- deeppavlov_agent/core/state_manager.py
+++ deeppavlov_agent/core/state_manager.py
@@ -89,6 +89,7 @@ class StateManager:
         if isinstance(dialog.utterances[-1], HumanUtterance):
             dialog.add_bot_utterance()
         dialog.utterances[-1].text = payload['text']
+        dialog.utterances[-1].orig_text = payload['text']
         dialog.utterances[-1].active_skill = label
         dialog.utterances[-1].confidence = 0
         dialog.utterances[-1].annotations = payload['annotations']
