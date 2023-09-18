ccd0b6ef1497502e67b2fc97719f0142cb73909f
dilyararimovna <dilyara.rimovna@gmail.com>
2023-09-18 15:35:31 +0400

    fix: debug_output by key and add annotations

--- deeppavlov_agent/state_formatters/output_formatters.py
+++ deeppavlov_agent/state_formatters/output_formatters.py
@@ -12,12 +12,16 @@ def http_api_output_formatter(payload: Dict):
 
 
 def http_debug_output_formatter(payload: Dict):
-    return {
+    result = {
         'dialog_id': payload['dialog_id'],
         'utt_id': payload['utterances'][-1]['utt_id'],
         'user_id': payload['human']['user_external_id'],
         'response': payload['utterances'][-1]['text'],
         'active_skill': payload['utterances'][-1]['active_skill'],
-        'debug_output': payload['utterances'][-2]['hypotheses'],
         'attributes': payload['utterances'][-1].get('attributes', {})
     }
+    if payload["utterances"][-2].get("attributes", {}).get("debug_output", False):
+        result["debug_output"]["hypotheses"] = payload['utterances'][-2]['hypotheses']
+        result["debug_output"]["annotations"] = payload['utterances'][-2]['annotations']
+
+    return result
